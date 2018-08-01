572c67c864c7ce85c3995c4e67e158ce1d6c1cc4
root <root@localhost.localdomain>
2018-08-01 01:37:12 -0400

    fix

--- actions/python-action.py
+++ actions/python-action.py
@@ -3,9 +3,9 @@ from st2actions.runners.pythonrunner import Action
 
 class CreateTicket(Action):
     def run(self, param1, param2, param3):
-        print("param1 " + str(param1))
-        print("param1 " + str(param1))
-        print("param1 " + str(param1))
+        print('param1 ')
+        #print("param1 " + str(param1))
+        #print("param1 " + str(param1))
         return (True, "Action run is successfull")
 
 
