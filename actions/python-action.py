01d43a096c316f7409cef1fef1f189b89d495352
namesh <namesh.sanjitha@pearson.com>
2018-08-01 00:56:39 -0500

    fix

--- actions/python-action.py
+++ actions/python-action.py
@@ -1,10 +1,10 @@
 import sys
-#from st2actions.runners.pythonrunner import Action
-from st2common.runners.base_action import Action
+from st2actions.runners.pythonrunner import Action
+#from st2common.runners.base_action import Action
 
 class CreateTicket(Action):
-    def run(self, param1, param2, param3):
-        print('param1 ')
+    def run(self, message):
+        print(message)
         #print("param1 " + str(param1))
         #print("param1 " + str(param1))
         return (True, "Action run is successfull")
